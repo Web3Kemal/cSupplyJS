459439216116ffe0f94c1ee5ee1ab081159661d7
Kemal Cleva <kemalcleva69@gmail.com>
2023-05-29 20:40:31 +0200

    Create cSupplyCapo.js

---
+++ cSupplyCapo.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  try {
+    const totalSupply = await getTotalSupply();
+    const burnedSupply = await getBurnedSupply();
+    const circulatingSupply = totalSupply - burnedSupply;
+    res.status(200).send({
+      status: '1',
+      message: 'OK',
+      result: String(circulatingSupply),
+    });
+  } catch (err) {
+    res.status(500).send({
+      status: '0',
+      message: 'Error fetching data',
+      result: null,
+    });
+  }
+};
+
+async function getTotalSupply() {
+  const response = await axios.get(
+    'https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0x922722e9ef614ec9a3e94b78496e92abfbb5a624&apikey=YEMEFWMH1EGKM3IG79DI7UB2FRBQ9KFAFD'
+  );
+  return BigInt(response.data.result);
+}
+
+async function getBurnedSupply() {
+  const response = await axios.get(
+    'https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x922722e9ef614ec9a3e94b78496e92abfbb5a624&address=0x000000000000000000000000000000000000dead&tag=latest&apikey=YEMEFWMH1EGKM3IG79DI7UB2FRBQ9KFAFD'
+  );
+  return BigInt(response.data.result);
+}
